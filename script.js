992cad812bad8c5e5073a6c363618440c26930b1
karimetauil <karimeupd@hotmail.com>
2023-05-09 14:58:20 -0300

    PROJETO DEV CONTABIL

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let formulario = document.getElementById("formulario-contato")
+              
+function chameOFormulario(){
+    formulario.className = "mostrar-formulario"
+    console.log("cliquei no botao de contato")
+}
